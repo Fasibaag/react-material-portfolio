84089ce9950b87195cb44dc8739b4e6d20e8359c
Jordi Talens <talensjr@gmail.com>
2016-06-24 19:37:20 +0200

    added serve-favicon

--- server/index.js
+++ server/index.js
@@ -1,11 +1,13 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const favicon = require('serve-favicon');
 const app = express();
 
 const server = http.createServer(app);
 const port = process.env.PORT || 3000;
 
+app.use(favicon(__dirname + '/../favicon.ico'));
 app.use(express.static(__dirname + '/../public'));
 app.use('/style', express.static(__dirname + '/../style'));
 app.use('/assets', express.static(__dirname + '/../assets'));
